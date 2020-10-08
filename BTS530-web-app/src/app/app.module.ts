ca6595b66481d394c40f3e350e1a9f569a5a68b8
Kevin <kwshearsmith@myseneca.ca>
2020-10-08 01:26:14 -0400

    Update to backend login functionality and angular app login page

--- BTS530-web-app/src/app/app.module.ts
+++ BTS530-web-app/src/app/app.module.ts
@@ -2,12 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from "@angular/forms";
 import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+
+
+import { JwtModule } from "@auth0/angular-jwt";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register.component';
 import { HomeComponent } from './home.component';
 import { LoginComponent } from './login.component';
+
+import { AuthService } from './auth.service';
+import { GuardAuthService } from './guard-auth.service';
+import { InterceptTokenService } from "./intercept-token.service";
+import { DataModelManagerService } from "./data-model-manager.service";
+
+export function tokenGetter() {
+  return localStorage.getItem('access_token');
+}
 
 @NgModule({
   declarations: [
@@ -20,9 +33,24 @@ import { LoginComponent } from './login.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: tokenGetter,
+        authScheme: 'JWT'
+      }
+    })
   ],
-  providers: [],
+  providers: [
+    AuthService,
+    DataModelManagerService,
+    GuardAuthService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: InterceptTokenService,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- BTS530-web-app/src/app/login.component.ts
+++ BTS530-web-app/src/app/login.component.ts
@@ -3,7 +3,9 @@ import { Router } from "@angular/router";
 import { NgForm } from '@angular/forms';
 
 import { DataModelManagerService } from "./data-model-manager.service";
+import { JwtHelperService } from '@auth0/angular-jwt';
 
+import { AuthService } from './auth.service';
 import { Credentials } from "./data-model-classes";
 
 @Component({
@@ -15,18 +17,50 @@ export class LoginComponent implements OnInit {
 
   // Data holding properties
   userCredentials: Credentials;
+  loginError: string;
+  loginStatus: boolean;
 
+  // Token
+  tokenRaw?: string;
+  tokenDecoded?: any;
+  tokenIssuedTimestamp: any;
 
-  constructor(private m: DataModelManagerService, private router: Router ) { 
+  constructor(
+    
+    //private m: DataModelManagerService, 
+    private a: AuthService,
+    private router: Router,
+    private jwtHelper: JwtHelperService
+
+    ) { 
+
+    // Fetch the token from the browser's local storage
+    //this.tokenRaw = localStorage.getItem('access_token');
+
     this.userCredentials = new Credentials();
+    this.userCredentials.username = '';
+    this.userCredentials.password = '';
+
+    this.loginStatus = null;
+    this.loginError = '';
   }
 
   ngOnInit(): void {
-  }
 
-  loginAttempt(): void {
-    this.m.apiUserLogin(this.userCredentials).subscribe(u => {
+  }
+  
+  onSubmit(): void {
+    //console.log(this.userCredentials);
+    
+    
+
+
+    
+
+    this.a.login(this.userCredentials).subscribe(u => {
       console.log(u);
+      this.loginStatus = u.loginStatus;
+      
       
     });
 
